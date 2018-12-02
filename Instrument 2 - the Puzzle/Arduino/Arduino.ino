aa11f524624be28cf5ec00deb719bac76855b2d1
Omid <omid_ettehadi@yahoo.com>
2018-12-01 22:44:45 -0500

    Updated the Puzzle - Version 1
    Check the six items and see which one is placed correctly.
    Turn one the same number of LEDs as Items placed.
    Send the status of all switches to the P5.
    Then P5 sends the status of all switches to PubNub.

--- Instrument 2 - the Puzzle/Arduino/Arduino.ino
+++ Instrument 2 - the Puzzle/Arduino/Arduino.ino
@@ -4,12 +4,152 @@
    by: April De Zen, & Veda Adnani, & Omid Ettehadi
 */
 
+// Libraries
+#include <ArduinoJson.h>
+
+// Definitions
+#define SWITCH1pin 2
+#define SWITCH2pin 3
+#define SWITCH3pin 4
+#define SWITCH4pin 5
+#define SWITCH5pin 6
+#define SWITCH6pin 7
+
+#define LED1pin 8
+#define LED2pin 9
+#define LED3pin 10
+#define LED4pin 11
+#define LED5pin 12
+#define LED6pin 13
+
+// Variables
+int switch1val;
+int switch2val;
+int switch3val;
+int switch4val;
+int switch5val;
+int switch6val;
+
+// Sampling
+unsigned long lastSend;
+int sendRate = 500;
+
 void setup() 
 {
-
+  Serial.begin(9600);
+  
+  // Assign pins
+  pinMode(SWITCH1pin, INPUT_PULLUP);
+  pinMode(SWITCH2pin, INPUT_PULLUP);
+  pinMode(SWITCH3pin, INPUT_PULLUP);
+  pinMode(SWITCH4pin, INPUT_PULLUP);
+  pinMode(SWITCH5pin, INPUT_PULLUP);
+  pinMode(SWITCH6pin, INPUT_PULLUP);
+  pinMode(LED1pin, OUTPUT);
+  pinMode(LED2pin, OUTPUT);
+  pinMode(LED3pin, OUTPUT);
+  pinMode(LED4pin, OUTPUT);
+  pinMode(LED5pin, OUTPUT);
+  pinMode(LED6pin, OUTPUT);
 }
 
 void loop() 
 {
+  // Set switch values to zero if they are open, and one if they are one
+  if ( digitalRead(SWITCH1pin) == HIGH ) { switch1val = 0; } else { switch1val = 1; }
+  if ( digitalRead(SWITCH2pin) == HIGH ) { switch2val = 0; } else { switch2val = 1; }
+  if ( digitalRead(SWITCH3pin) == HIGH ) { switch3val = 0; } else { switch3val = 1; }
+  if ( digitalRead(SWITCH4pin) == HIGH ) { switch4val = 0; } else { switch4val = 1; }
+  if ( digitalRead(SWITCH5pin) == HIGH ) { switch5val = 0; } else { switch5val = 1; }
+  if ( digitalRead(SWITCH6pin) == HIGH ) { switch6val = 0; } else { switch6val = 1; }
 
+  // Check how many switches are on, turn that many LEDs on
+  // See how many switches are connected
+  int sum = switch1val + switch2val + switch3val + switch4val + switch5val + switch6val;
+    
+  // If no switch is connected turn all LEDs off
+  if (switch1val == LOW && switch2val == LOW && switch3val == LOW 
+  && switch4val == LOW && switch5val == LOW && switch6val == LOW)
+  {
+    digitalWrite(LED1pin, LOW);
+    digitalWrite(LED2pin, LOW);
+    digitalWrite(LED3pin, LOW);
+    digitalWrite(LED4pin, LOW);
+    digitalWrite(LED5pin, LOW);
+    digitalWrite(LED6pin, LOW);
+
+  } 
+  else if ( sum == 1 ) 
+  {
+    digitalWrite(LED1pin, HIGH);
+    digitalWrite(LED2pin, LOW);
+    digitalWrite(LED3pin, LOW);
+    digitalWrite(LED4pin, LOW);
+    digitalWrite(LED5pin, LOW);
+    digitalWrite(LED6pin, LOW); 
+  }
+  else if ( sum == 2 ) 
+  {
+    digitalWrite(LED1pin, HIGH);
+    digitalWrite(LED2pin, HIGH);
+    digitalWrite(LED3pin, LOW);
+    digitalWrite(LED4pin, LOW);
+    digitalWrite(LED5pin, LOW);
+    digitalWrite(LED6pin, LOW);
+  }
+  else if ( sum == 3 ) 
+  {
+    digitalWrite(LED1pin, HIGH);
+    digitalWrite(LED2pin, HIGH);
+    digitalWrite(LED3pin, HIGH);
+    digitalWrite(LED4pin, LOW);
+    digitalWrite(LED5pin, LOW);
+    digitalWrite(LED6pin, LOW); 
+  }
+  else if ( sum == 4 ) 
+  {
+    digitalWrite(LED1pin, HIGH);
+    digitalWrite(LED2pin, HIGH);
+    digitalWrite(LED3pin, HIGH);
+    digitalWrite(LED4pin, HIGH);
+    digitalWrite(LED5pin, LOW);
+    digitalWrite(LED6pin, LOW); 
+  }
+  else if ( sum == 5 ) 
+  {
+    digitalWrite(LED1pin, HIGH);
+    digitalWrite(LED2pin, HIGH);
+    digitalWrite(LED3pin, HIGH);
+    digitalWrite(LED4pin, HIGH);
+    digitalWrite(LED5pin, HIGH);
+    digitalWrite(LED6pin, LOW);
+  }
+  else if (sum == 6 ) 
+  {
+    digitalWrite(LED1pin, HIGH);
+    digitalWrite(LED2pin, HIGH);
+    digitalWrite(LED3pin, HIGH);
+    digitalWrite(LED4pin, HIGH);
+    digitalWrite(LED5pin, HIGH);
+    digitalWrite(LED6pin, HIGH);  
+  }
+
+  // Send data through Serial Connection to the P5
+  if( millis()-lastSend >= sendRate )
+  {
+    DynamicJsonBuffer messageBuffer(512);                   // Create the Buffer for the JSON object        
+    JsonObject& p5Send = messageBuffer.createObject();      // Create a JsonObject variable in that buffer       
+    
+    p5Send["switch1"] = switch1val;                         // Assign switch1val to the key "switch1" in the json object
+    p5Send["switch2"] = switch2val;                         // Assign switch2val to the key "switch2" in the json object
+    p5Send["switch3"] = switch3val;                         // Assign switch3val to the key "switch3" in the json object
+    p5Send["switch4"] = switch4val;                         // Assign switch4val to the key "switch4" in the json object
+    p5Send["switch5"] = switch5val;                         // Assign switch5val to the key "switch5" in the json object
+    p5Send["switch6"] = switch6val;                         // Assign switch6val to the key "switch6" in the json object
+    
+    p5Send.printTo(Serial);                                 // Print JSON object as a string
+    Serial.println();                                       // Print a \n character to the serial port to distinguish between objects
+
+    lastSend = millis();
+  }
 }
--- Instrument 2 - the Puzzle/P5/sketch.js
+++ Instrument 2 - the Puzzle/P5/sketch.js
@@ -1,5 +1,17 @@
 /* -------------------- Variables -------------------- */
+// Server variables
+var dataServer;
+var pubKey = 'pub-c-0a3b6b6e-0ef0-4b11-b8eb-1a597dc2331f';
+var subKey = 'sub-c-c92fc548-f343-11e8-b085-b2b44c5b7fba';
+var musicChannelName = "musicChannel";
 
+// Serial Connection variables
+var serial;       						// Serial port object
+var serialPortName = "COM8";			// Serial port name
+var ardSend = {};						// JSON variable
+
+// Variables
+var switch1val, switch2val, switch3val, switch4val, switch5val, switch6val;
 
 /* -------------------- Functions -------------------- */
 function setup() 
@@ -7,17 +19,69 @@ function setup()
     getAudioContext().resume();
     createCanvas(windowWidth, windowHeight);
     background(0);
+    
+    // Initialize pubnub
+    dataServer = new PubNub(
+    {
+        publish_key   : pubKey,
+        subscribe_key : subKey,  
+        ssl: true
+    });
+    
+    // Attach callbacks to the pubnub object to handle messages and connections
+    dataServer.addListener({message: readIncoming})
+    dataServer.subscribe({channels: [musicChannelName]});
+
+    // Setting up the serial port
+    serial = new p5.SerialPort();		// Create the serial port object
+    serial.open(serialPortName);		// Open the serialport
+    serial.on('open',ardCon);			// Open the socket connection
+	serial.on('data',dataReceived);		// Call dataReceived when new data is received
 }
 function draw() {}
 function windowResized() {}
 
 /* -------------------- PubNub -------------------- */
 // Send data to pubnub
-function sendTheMessage() {}
+function sendTheMessage() 
+{
+	dataServer.publish(
+    {
+        channel: musicChannelName,
+        message: 
+        {
+            switch1: switch1val,
+			switch2: switch2val,
+			switch3: switch3val,
+			switch4: switch4val,
+			switch5: switch5val,
+			switch6: switch6val,
+        }
+    });
+}
 
 // Read data from pubnub
 function readIncoming(inMessage) {}
 
 /* -------------------- Arduino -------------------- */
 // Recieve data from Arduino
-function dataReceived () {}
+function dataReceived ()
+{
+	var rawData = serial.readStringUntil('\r\n');		// Read the incoming string until it sees a newline
+	//console.log(rawData);
+	
+	if( rawData.length > 1 )								// Check that there is something in the string
+	{
+		switch1val = JSON.parse(rawData).switch1;		// Set variable switch1val to switch1 from the incoming string 
+		switch2val = JSON.parse(rawData).switch2;		// Set variable switch2val to switch2 from the incoming string 
+		switch3val = JSON.parse(rawData).switch3;		// Set variable switch3val to switch3 from the incoming string 
+		switch4val = JSON.parse(rawData).switch4;		// Set variable switch4val to switch4 from the incoming string 
+		switch5val = JSON.parse(rawData).switch5;		// Set variable switch5val to switch5 from the incoming string 
+		switch6val = JSON.parse(rawData).switch6;		// Set variable switch6val to switch6 from the incoming string
+		sendTheMessage();
+	}
+}
+function ardCon ()
+{
+    console.log("connected to the Arduino Micro! Let's Go");
+}
