297fb239e43d4d6ebd7f4e95dcd759c47e9ec50d
Omid <omid_ettehadi@yahoo.com>
2018-12-08 19:05:42 -0500

    Updated the Display P5 - Version 1
    Play notes depending on which key is pressed

--- the Display/Arduino/Arduino.ino
+++ the Display/Arduino/Arduino.ino
@@ -4,12 +4,205 @@
    by: April De Zen, & Veda Adnani, & Omid Ettehadi
 */
 
+// Libraries
+#include <ArduinoJson.h>
+#include <Adafruit_NeoPixel.h>
+
+// Definitions
+#define LED1pin 12
+#define LED2pin 11
+#define LED3pin 10
+#define INSTRUMENT_1_NUM_LEDS 30
+#define INSTRUMENT_2_NUM_LEDS 60
+#define INSTRUMENT_3_NUM_LEDS 60
+
+// Objects
+Adafruit_NeoPixel stripOne = Adafruit_NeoPixel(INSTRUMENT_1_NUM_LEDS, LED1pin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripTwo = Adafruit_NeoPixel(INSTRUMENT_2_NUM_LEDS, LED2pin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripThree = Adafruit_NeoPixel(INSTRUMENT_3_NUM_LEDS, LED3pin, NEO_GRB + NEO_KHZ800);
+
+// Variables
+int LedOneStatus , LedTwoStatus, LedThreeStatus;
+
+// Sampling
+unsigned long lastSend;
+int sendRate = 2000;
+
 void setup() 
 {
-  
+  // Begin serial connection
+  Serial.begin(9600);
+
+  stripOne.begin();
+  stripOne.setBrightness(200);
+  stripOne.show();
+
+  delay(1000);
+
+  stripTwo.begin();
+  stripTwo.setBrightness(200);
+  stripTwo.show();
+
+  delay(1000);
+
+  stripThree.begin();
+  stripThree.setBrightness(200);
+  stripThree.show();
+
+  delay(1000);
+
+  for ( int i = 0; i < 30; i++ )
+  {
+    stripOne.setPixelColor(i, 255, 255, 255);
+    stripOne.show();
+  }
+  for ( int i = 0; i < 30; i++ )
+  {
+    stripOne.setPixelColor(i, 0, 0, 0);
+    stripOne.show();
+  }
+  for ( int i = 0; i < 60; i++ )
+  {
+    stripTwo.setPixelColor(i, 255, 255, 255);
+    stripTwo.show();
+    stripThree.setPixelColor(i, 255, 255, 255);
+    stripThree.show();
+  }
+  for ( int i = 0; i < 60; i++ )
+  {
+    stripTwo.setPixelColor(i, 0, 0, 0);
+    stripTwo.show();
+    stripThree.setPixelColor(i, 0, 0, 0);
+    stripThree.show();
+  }
 }
 
 void loop() 
 {
-
+  if( millis()-lastSend >= sendRate )
+  {
+    if ( LedOneStatus == 1 )
+    {
+      for ( int i = 0; i < 30; i++ )
+      {
+        stripOne.setPixelColor(i, 0, 255, 0);
+        stripTwo.show();
+      }
+      for ( int i = 0; i < 30; i++ )
+      {
+        stripOne.setPixelColor(i, 0, 0, 0);
+        stripOne.show();
+      }
+    } else {
+      for ( int i = 0; i < 30; i++ )
+      {
+        stripOne.setPixelColor(i, 255, 255, 255);
+        stripTwo.show();
+      }
+      for ( int i = 0; i < 30; i++ )
+      {
+        stripOne.setPixelColor(i, 0, 0, 0);
+        stripOne.show();
+      }
+    }
+    lastSend = millis();
+  }
+  DynamicJsonBuffer messageBuffer(200);               // Buffer for the JSON object        
+  JsonObject& p5Read = messageBuffer.parse(Serial);   // JsonObject attaches to incoming serial message
+  
+  LedOneStatus = p5Read["LEDOneStatus"];              // Assign "PuzzleStatus" from the json object to LedOneStatus
+  LedTwoStatus = p5Read["LEDTwoStatus"];              // Assign "BallOneStatus" from the json object to LedTwoStatus
+  LedThreeStatus = p5Read["LEDThreeStatus"];          // Assign "BallTwoStatus" from the json object to LedThreeStatus
+    
+    
+  /*if ( LedOneStatus == 1 )
+  {
+    for ( int i = 0; i < 30; i++ )
+    {
+      stripOne.setPixelColor(i, 255, 255, 255);
+      stripTwo.show();
+    }
+    for ( int i = 0; i < 30; i++ )
+    {
+      stripOne.setPixelColor(i, 0, 0, 0);
+      stripOne.show();
+    }
+  } else {
+    for ( int i = 0; i < 30; i++ )
+    {
+      stripOne.setPixelColor(i, 255, 255, 255);
+      stripTwo.show();
+    }
+    for ( int i = 0; i < 30; i++ )
+    {
+      stripOne.setPixelColor(i, 0, 0, 0);
+      stripOne.show();
+    }
+  }*/
+    
+  if ( LedTwoStatus == 0 && LedThreeStatus == 0 )
+  {
+    /*for ( int i = 0; i < 60; i++ )
+    {
+      stripTwo.setPixelColor(i, 255, 255, 255);
+      stripTwo.show();
+      stripThree.setPixelColor(i, 255, 255, 255);
+      stripThree.show();
+    }
+    for ( int i = 0; i < 60; i++ )
+    {
+      stripTwo.setPixelColor(i, 0, 0, 0);
+      stripTwo.show();
+      stripThree.setPixelColor(i, 0, 0, 0);
+      stripThree.show();
+    }*/
+  } else if ( LedTwoStatus == 0 && LedThreeStatus == 1 )
+  {
+    for ( int i = 0; i < 60; i++ )
+    {
+      stripTwo.setPixelColor(i, 255, 255, 255);
+      stripTwo.show();
+      stripThree.setPixelColor(i, 0, 128, 128);
+      stripThree.show();
+    }
+    for ( int i = 0; i < 60; i++ )
+    {
+      stripTwo.setPixelColor(i, 0, 0, 0);
+      stripTwo.show();
+      stripThree.setPixelColor(i, 0, 0, 0);
+      stripThree.show();
+    }
+  } else if ( LedTwoStatus == 1 && LedThreeStatus == 0 )
+  {
+    for ( int i = 0; i < 60; i++ )
+    {
+      stripTwo.setPixelColor(i, 148, 0, 211);
+      stripTwo.show();
+      stripThree.setPixelColor(i, 255, 255, 255);
+      stripThree.show();
+    }
+    for ( int i = 0; i < 60; i++ )
+    { 
+      stripTwo.setPixelColor(i, 0, 0, 0);
+      stripTwo.show();
+      stripThree.setPixelColor(i, 0, 0, 0);
+      stripThree.show();
+    }
+  } else if ( LedTwoStatus == 1 && LedThreeStatus == 1 )
+  {
+    for ( int i = 0; i < 60; i++ )
+    {
+      stripTwo.setPixelColor(i, 148, 0, 211);
+      stripTwo.show();
+      stripThree.setPixelColor(i, 0, 128, 128);
+      stripThree.show();
+    }
+    for ( int i = 0; i < 60; i++ )
+    {
+      stripTwo.setPixelColor(i, 0, 0, 0);
+      stripTwo.show();
+      stripThree.setPixelColor(i, 0, 0, 0);
+      stripThree.show();
+    }
+  }
 }
--- the Display/P5/sketch.js
+++ the Display/P5/sketch.js
@@ -10,11 +10,34 @@ var serial;       						// Serial port object
 var serialPortName = "COM7";			// Serial port name
 var ardSend = {};						// JSON variable
 
+// PubNub Data
+var BallOne_averageVelocity = 0, Prev_BallOne_averageVelocity = 0;
+var BallTwo_averageVelocity= 0, Prev_BallTwo_averageVelocity = 0;
+var switch1val = 0, switch2val = 0, switch3val = 0,
+	switch4val = 0, switch5val = 0, switch6val = 0;
+
+// Arduino Data
+var PuzzleStatus = 0, BallOneStatus = 0, BallTwoStatus = 0;
+
+// Audios
+var BigCircleAudio, SmallCircleAudio;
+var BigRectAudio, SmallRectAudio;
+var TrigAudio, TrapAudio;
+var BallOneAudio, BallTwoAudio;
+var volume1 , volume2, volume3, volume4, volume5, volume6;
 
 /* -------------------- Functions -------------------- */
 function preload(){
 	// Preload sounds
-	//soundArticle1 = loadSound("audios/.mp3");
+	BigCircleAudio = loadSound("audios/KickTrack.wav");
+	TrapAudio = loadSound("audios/FluteTrack1.wav");
+	SmallRectAudio = loadSound("audios/BassTrack.wav");
+	SmallCircleAudio = loadSound("audios/FluteTrack2.wav");
+	TrigAudio = loadSound("audios/PikesTrack.wav");
+	BigRectAudio = loadSound("audios/VocalTrack.wav");
+	
+	BallOneAudio = loadSound("audios/GongTrack.wav");
+	BallTwoAudio = loadSound("audios/TambourineTrack.wav");
 }
 function setup() 
 {
@@ -31,21 +54,103 @@ function setup()
     });
     
     // Attach callbacks to the pubnub object to handle messages and connections
-    dataServer.addListener({ message: readIncoming, presence: whoisconnected })
+    dataServer.addListener({message: readIncoming})
     dataServer.subscribe({channels: [musicChannelName]});
 
-    // Setting up the serial port
+	/*
+	// Setting up the serial port
     serial = new p5.SerialPort();		// Create the serial port object
     serial.open(serialPortName);		// Open the serialport
     serial.on('open',ardCon);			// Open the socket connection
+	*/
 }
-
 function draw() 
 {
-    
+	// -------------------- thePuzzle -------------------- //
+	// Play all tracks on a constant loop
+	if ( !BigCircleAudio.isPlaying() ) 
+	{
+		memory = 0;
+		BigCircleAudio.play();
+    }
+	if ( !TrapAudio.isPlaying() ) 
+	{
+		TrapAudio.play();
+    }
+	if ( !SmallRectAudio.isPlaying() ) 
+	{
+		SmallRectAudio.play();  
+    }
+	if ( !SmallCircleAudio.isPlaying() ) 
+	{
+		SmallCircleAudio.play();
+	}
+	if ( !TrigAudio.isPlaying() ) 
+	{
+		TrigAudio.play();  
+    }
+	if ( !BigRectAudio.isPlaying() ) 
+	{
+		BigRectAudio.play();
+    }
+	
+	// Set the track's volume according to switches
+	if ( switch1val == 1 ) { volume1 = 1; } else { volume1 = 0; }
+    BigCircleAudio.setVolume(volume1);  
+	if ( switch2val == 1 ) { volume2 = 1; } else { volume2 = 0; }
+    TrapAudio.setVolume(volume2);
+	if ( switch3val == 1 ) { volume3 = 1; } else { volume3 = 0; }
+    SmallRectAudio.setVolume(volume3);
+	if ( switch4val == 1 ) { volume4 = 1; } else { volume4 = 0; }
+	SmallCircleAudio.setVolume(volume4);  
+	if ( switch5val == 1 ) { volume5 = 1; } else { volume5 = 0; }  
+    TrigAudio.setVolume(volume5);
+	if ( switch6val == 1 ) { volume6 = 1; } else { volume6 = 0; }
+    BigRectAudio.setVolume(volume6);  
+	
+	// Set the status for arduino
+	PuzzleStatus = (switch6val || switch5val || switch4val || switch3val|| switch2val|| switch1val);
+	
+	// -------------------- theBalls -------------------- //
+	// the Ball 1
+	// If the velocity is higher than 20 and the value is not the same as last trial play the sound
+	// Set BallOneStatus to 1 if the music is playing for the arduino to show the light
+	if ( BallOne_averageVelocity >= 20 &&  BallOne_averageVelocity != Prev_BallOne_averageVelocity){
+		Prev_BallOne_averageVelocity = BallOne_averageVelocity;	
+		if ( !BallOneAudio.isPlaying() )
+		{
+			BallOneAudio.play();
+			BallOneAudio.setVolume(1); 
+			BallOneStatus = 1;
+		} 
+	} else {
+		BallOneStatus = 0;
+	}
+	// the Ball 2
+	// If the velocity is higher than 20 and the value is not the same as last trial play the sound
+	// Set BallTwoStatus to 1 if the music is playing for the arduino to show the light
+	if ( BallTwo_averageVelocity >= 20 &&  BallTwo_averageVelocity != Prev_BallTwo_averageVelocity)
+	{
+		Prev_BallTwo_averageVelocity = BallTwo_averageVelocity;
+		if ( !BallTwoAudio.isPlaying() )
+		{
+			BallTwoAudio.play();
+			BallTwoAudio.setVolume(1); 
+			BallTwoStatus = 1;
+		} 
+	} else
+	{
+		BallTwoStatus = 0;
+	}
+	
+	// Send data to arduino
+	sendData();
 }
-
-function windowResized() {}
+function windowResized() 
+{
+    resizeCanvas(windowWidth, windowHeight);
+	background(0); 
+}
 
 /* -------------------- PubNub -------------------- */
 // Send data to pubnub
@@ -56,35 +161,42 @@ function readIncoming(inMessage)
 {
     if(inMessage.channel == musicChannelName)
     {
-		if ( inMessage.message.whoAmI == "Game" )
+		if ( inMessage.message.who == "thePuzzle" )
 		{
-
-		} else if ( inMessage.message.whoAmI == "Piano" ) 
+			switch1val = inMessage.message.switch1;
+			switch2val = inMessage.message.switch2;
+			switch3val = inMessage.message.switch3;
+			switch4val = inMessage.message.switch4;
+			switch5val = inMessage.message.switch5;
+			switch6val = inMessage.message.switch6;
+		} 
+		else if ( inMessage.message.who == "theBallOne" ) 
 		{
-
-		} else if ( inMessage.message.whoAmI == "Ball" ) 
+			BallOne_averageVelocity = inMessage.message.averageVelocity;
+		} 
+		else if ( inMessage.message.who == "theBallTwo" ) 
 		{
-
+			BallTwo_averageVelocity = inMessage.message.averageVelocity;
 		}
     }
 }
 
-function whoisconnected(connectionInfo) {}
-
-/* -------------------- Arduino -------------------- */
+/* -------------------- Arduino --------------------
 // Send data to Arduino
 function sendData()
-{
-    ardSend.LEDStatus = 							// Add the value to the LEDStatus parameter on the json object
-    
-	var sendString = JSON.stringify(ardSend);   	// Convert the json to a string  
-    console.log(sendString)
+{	
+    ardSend.LEDOneStatus = PuzzleStatus;						// Add the value to the LEDOneStatus parameter on the json object
+    ardSend.LEDTwoStatus = BallOneStatus;						// Add the value to the LEDTwoStatus parameter on the json object
+	ardSend.LEDThreeStatus = BallTwoStatus;						// Add the value to the LEDThreeStatus parameter on the json object
 
-    serial.write(sendString);                  		// Send it over the serial port    
-    serial.write('\n');                           	// Write a new line character
+	var sendString = JSON.stringify(ardSend);   				// Convert the json to a string  
+    //console.log(sendString)
+	
+	serial.write(sendString);                  					// Send it over the serial port    
+    serial.write('\n');                           				// Write a new line character	
 }
 
 function ardCon()
 {
     console.log("connected to the Arduino Micro! Let's Go");
-}
+}*/
